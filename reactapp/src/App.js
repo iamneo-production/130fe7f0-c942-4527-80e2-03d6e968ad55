ab9e9ed636988448022e0169942f5b3dbf59e309
Deepak-john-08 <deepakjohndj12@gmail.com>
2022-09-30 11:35:02 +0000

    initial

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
+import Login from './Login';
+import Dashboard from './Dashboard';
+import AdminDashboard from './AdminDashboard';
+import Home from './Home';
+import SignUp from './SignUp';
+import FetchApi from './FetchApi';
+import Login1 from './Login1';
+import Course from './Course';
+import CourseAdd from './CourseAdd';
+import CourseDel from './CourseDel';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Abacus Academy</NavLink>
+            
+            
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+              <Route path="/signup" component={SignUp} />
+              <Route path="/fetchapi" component={FetchApi}/>       
+              <Route path="/login1" component={Login1}/>
+              <Route path="/admindashboard" component={AdminDashboard}/> 
+              <Route path="/course" component={Course}/>     
+              <Route path="/courseadd" component={CourseAdd}/>
+              <Route path="/coursedelete" component={CourseDel}/>
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
